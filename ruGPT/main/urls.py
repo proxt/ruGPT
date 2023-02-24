8c20b3b74cc928c7f243ab85cf208027b216e4c8
skyrimxts@gmail.com <32ipubib5429>
2023-02-24 12:33:50 +0300

    initial

--- ruGPT/main/urls.py
+++ ruGPT/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about)
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news-detail')
 ]
